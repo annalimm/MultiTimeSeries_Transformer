d07ec0b2dedc9b1faf7c56dfd7f50d47cee2f506
annalimm <anyamerkushina@gmail.com>
2023-11-02 18:35:14 +0100

    .

--- preprocessing.py
+++ preprocessing.py
@@ -1,41 +1,18 @@
-{
-  "nbformat": 4,
-  "nbformat_minor": 0,
-  "metadata": {
-    "colab": {
-      "provenance": [],
-      "authorship_tag": "ABX9TyMdQnLmjgY8jzCz1EyiXOYs",
-      "include_colab_link": true
-    },
-    "kernelspec": {
-      "name": "python3",
-      "display_name": "Python 3"
-    },
-    "language_info": {
-      "name": "python"
-    }
-  },
-  "cells": [
-    {
-      "cell_type": "markdown",
-      "metadata": {
-        "id": "view-in-github",
-        "colab_type": "text"
-      },
-      "source": [
-        "<a href=\"https://colab.research.google.com/github/annalimm/MultiTimeSeries_Transformer/blob/main/preprocessing.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
-      ]
-    },
-    {
-      "cell_type": "code",
-      "execution_count": null,
-      "metadata": {
-        "id": "bPh66R-sAX2Y"
-      },
-      "outputs": [],
-      "source": [
-        "rrr"
-      ]
-    }
-  ]
-}
+def df_split(df):
+    # we maintain chronological order for time-series
+    train_samples = int(len(df) * 0.8)
+    val_samples = int(len(df) * 0.1)
+
+    df_train = df[:train_samples]
+    df_val = df[train_samples: train_samples + val_samples]
+    df_test = df[train_samples + val_samples:]
+
+    # we csn remove date column, because the time information is now embedded in a different form  
+    df_train = df_train.loc[:, df_train.columns != 'Time']
+    df_val = df_val.loc[:, df_val.columns != 'Time']
+    df_test = df_test.loc[:, df_test.columns != 'Time']
+
+    print('Train df shape: {}'.format(df_train.shape))
+    print('Val df shape: {}'.format(df_val.shape))
+    print('Test df shape: {}'.format(df_test.shape))
+    return df_train, df_val, df_test
